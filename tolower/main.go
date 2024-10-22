b0eb22c2955dc935585530d471e67c2fccca4b9c
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-22 15:41:23 +0300

    tolower (conver lowecase letter to uppercase)

---
+++ tolower/main.go
@@ -0,0 +1,12 @@
+package main
+
+func toLower(low string) string {
+	result := ""
+	for _, char := range low {
+		if char >= 'a' && char <= 'z' {
+			char += 32
+		}
+		result += string(char)
+	}
+	return result
+}
