d84e111c78ddbb299535e90f972bf3fb959da2c8
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-29 11:06:07 +0200

    code was wrong , modified it a bit

--- vowel.go
+++ vowel.go
@@ -1,14 +1,29 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-func addAn(vowel []string) []string {
 
-	for i := 0; i < len(vowel); i++ {
-		if len(vowel[i]) > 0 && strings.Contains("aeiouh", string(vowel[i][0])) {
-       vowel[i] = "an " + vowel[i]
+
+func addAn(words []string) []string {
+	var modifiedWords []string
+
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+
+		// If word is "a" and the next word starts with a vowel or 'h', replace it with "an"
+		if word == "a" && i+1 < len(words) && len(words[i+1]) > 0 &&
+			strings.Contains("aeiouhAEIOUH", string(words[i+1][0])) {
+			modifiedWords = append(modifiedWords, "an")
+		} else {
+			modifiedWords = append(modifiedWords, word)
 		}
-
 	}
-   return removeTheSlice(vowel) 
+	return modifiedWords
 }
+
+
+
+
