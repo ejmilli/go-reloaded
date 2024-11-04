79b0488863e1a5128706b0da95cba5bc3cdc0112
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-04 16:00:23 +0200

    explaining the code

--- converthenumb.go
+++ converthenumb.go
@@ -10,3 +10,7 @@ func extractNum(s string) int {
 
 }
 
+/////////////   example  //////////////
+
+
+// supposedly i have "I am exciting (up 2)" it removes "2" and converts it into int 2 n then use it in labrat to actually convert to up, low, capitalize etc
